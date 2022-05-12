390d46bb12bf6004f208def863f5022ef3baee1b
surungo <surungo@gmail.com>
2022-05-12 18:31:43 -0300

    update

--- gupdate.sh
+++ gupdate.sh
@@ -2,5 +2,5 @@
 
 echo "git fetch -v --progress \"origin\""
 git fetch -v --progress "origin"
-echo "git pull"
-git pull
+echo "git pull --progress -v --no-rebase \"origin\""
+git pull --progress -v --no-rebase "origin"
