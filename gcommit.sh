cabe2180daf493cca8dfcc0e63b11fe868395f45
surungo <surungo@gmail.com>
2022-05-12 18:05:53 -0300

    ajuste

--- gcommit.sh
+++ gcommit.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "git fetch -v --progress \"origin\""
-git fetch -v --progress "origin"
-echo "git pull"
-git pull
+./gupdate.sh
+echo -n "Commit message: "
+read COMMIT_MSG
+echo "git commit -a -m  \"${COMMIT_MSG}\""
+git commit -a -m "${COMMIT_MSG}"
+echo "git push"
+git push
